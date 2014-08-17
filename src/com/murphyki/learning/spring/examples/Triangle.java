e134a2bb6108dc193047e60af0cdbfda54435e1d
kieran <kierantmurphy@gmail.com>
2014-08-17 12:33:11 +0100

    Refactoring

--- src/com/murphyki/learning/spring/examples/Triangle.java
+++ src/com/murphyki/learning/spring/examples/Triangle.java
@@ -1,8 +1,17 @@
 package com.murphyki.learning.spring.examples;
 
 public class Triangle {
+	private String type;
 	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
 	public void draw() {
-		System.out.println("Triangle.draw()");
+		System.out.println("Triangle.draw() is drawing a " + this.type + " triangle!");
 	}
 }
--- src/com/murphyki/learning/spring/examples/simple/Application.java
+++ src/com/murphyki/learning/spring/examples/simple/Application.java
@@ -1,11 +1,13 @@
-package com.murphyki.learning.spring.examples;
+package com.murphyki.learning.spring.examples.simple;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.murphyki.learning.spring.examples.Triangle;
+
 public class Application {
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("example1.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/murphyki/learning/spring/examples/simple/spring.xml");
 		Triangle triangle = (Triangle)context.getBean("triangle");
 		triangle.draw();
 	}
