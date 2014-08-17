c25015d71c53995be40052f2b0d015ab08247515
kieran <kierantmurphy@gmail.com>
2014-08-17 22:42:29 +0100

    Removed commented out code

--- src/com/murphyki/learning/spring/examples/SpringBeanFactory/Application.java
+++ src/com/murphyki/learning/spring/examples/SpringBeanFactory/Application.java
@@ -6,13 +6,7 @@ import org.springframework.core.io.FileSystemResource;
 
 public class Application {
 	public static void main(String[] args) {
-		//@SuppressWarnings("resource")
-		//ApplicationContext context = new ClassPathXmlApplicationContext(Application.class.getPackage().getName().replace(".", "/") + "/spring.xml");
-		//Triangle triangle = (Triangle)context.getBean("triangle");
-		//triangle.draw();
-		
 		BeanFactory factory = new XmlBeanFactory(new FileSystemResource(Application.class.getResource("spring.xml").getPath()));
-		
 		Triangle triangle = (Triangle)factory.getBean("triangle");
 		triangle.draw();
 	}
