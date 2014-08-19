a48760648fb6c5921e31c200b85969bff442b85f
kieran <kierantmurphy@gmail.com>
2014-08-19 12:39:58 +0100

    Refactoring

--- src/com/murphyki/learning/spring/examples/Simple/Application.java
+++ src/com/murphyki/learning/spring/examples/Simple/Application.java
@@ -1,4 +1,4 @@
-package com.murphyki.learning.spring.examples.simple;
+package com.murphyki.learning.spring.examples.Simple;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
