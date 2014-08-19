8c36583a34f842b66762487e5e0b7137c675caf1
kieran <kierantmurphy@gmail.com>
2014-08-19 22:34:40 +0100

    Refactoring

--- src/com/murphyki/learning/spring/examples/BeanPostProcessor/CustomBeanPostProcessor.java
+++ src/com/murphyki/learning/spring/examples/BeanPostProcessor/CustomBeanPostProcessor.java
@@ -6,18 +6,18 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 public class CustomBeanPostProcessor implements BeanPostProcessor {
 
 	@Override
-	public Object postProcessAfterInitialization(Object bean, String arg1) throws BeansException {
+	public Object postProcessAfterInitialization(Object bean, String name) throws BeansException {
 		System.out.println("CustomBeanPostProcessor.postProcessAfterInitialization()");
 		System.out.println("bean: " + bean.toString());
-		System.out.println("arg1: " + arg1);
+		System.out.println("arg1: " + name);
 		return bean;
 	}
 
 	@Override
-	public Object postProcessBeforeInitialization(Object bean, String arg1) throws BeansException {
+	public Object postProcessBeforeInitialization(Object bean, String name) throws BeansException {
 		System.out.println("CustomBeanPostProcessor.postProcessBeforeInitialization()");
 		System.out.println("bean: " + bean.toString());
-		System.out.println("arg1: " + arg1);
+		System.out.println("arg1: " + name);
 		return bean;
 	}
 
