3b199614cb6e0bb0fbffae763b5c852abd28e3aa
kieran <kierantmurphy@gmail.com>
2014-08-27 22:25:14 +0100

    Add BeanFactoryPostProcessor example

---
+++ src/com/murphyki/learning/spring/examples/BeanFactoryPostProcessor/CustomBeanFactoryPostProcessor.java
@@ -0,0 +1,14 @@
+package com.murphyki.learning.spring.examples.BeanFactoryPostProcessor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class CustomBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+	@Override
+	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+		System.out.println("CustomBeanFactoryPostProcessor.postProcessBeanFactory() called...");
+	}
+
+}
