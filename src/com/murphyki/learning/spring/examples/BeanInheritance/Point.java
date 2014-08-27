131cc5f7c4d35c862250f29a786451ad0baaa0ff
kieran <kierantmurphy@gmail.com>
2014-08-27 22:14:43 +0100

    Added BeanInheritance example

---
+++ src/com/murphyki/learning/spring/examples/BeanInheritance/Point.java
@@ -0,0 +1,29 @@
+package com.murphyki.learning.spring.examples.BeanInheritance;
+
+public class Point {
+	private int x;
+	private int y;
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		System.out.println("setX() called: " + x);
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		System.out.println("setY() called: " + y);
+		this.y = y;
+	}
+	
+	@Override
+	public String toString() {
+		return "(" + this.x + ", " + this.y + ")";
+	}
+}
